f1eef969399bebc80286cd1c688df616773c095e
mbouridev <95183215+mbouridev@users.noreply.github.com>
2021-12-07 18:36:00 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -5,15 +5,24 @@ function commencer()
         .then(data => data.json())
         .then(function(data)
             {
-                data = data['customers'];
+                data = data['customers']; // Récupère les données JSON des 'customers' (les clients)
 
-                // CRÉER UN TABLEAU AVEC LES NOMS DES UTILISATEURS
-                var users = [];
 
-                for(var index in data) {
-                    users[index] = data[index]['user_name'];
+                // OBTENIR LE NOM DES UTILISATEURS
+                // ***
+                var usersName = []; // Nouvelle variable pour stocker les noms dans un tableau
+
+
+                // REMPLISSAGE DU TABLEAU
+                // Boucle pour parcourir les données JSON de 'data'
+                for(var index in data)
+                {
+                    // Exemple: data[0]['user_name'] correspond au nom de
+                    // l'utilisateur ligne 0 des données JSON de 'data'
+                    usersName.push( data[index]['user_name'] ); // 'PUSH' ajoute une valeur au tableau
                 }
 
+                
                 // AFFICHAGE
                 console.log("Contenu du fichier JSON");
                 console.log(data);
